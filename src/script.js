dc94da5911e6a92de2b0331e4ca6d9ce05bd45c1
hazvyg <hgwembe@gmail.com>
2024-10-24 08:02:00 +0200

    added api keys

--- src/script.js
+++ src/script.js
@@ -3,7 +3,11 @@ let form = document.querySelector("#search-form");
 let cityInput = document.querySelector("#city-input");
 let cityDisplay = document.querySelector("#city-name");
 
-
+function searchCity{
+    let apiKey = "5af0c133bfa709dct64aabc68f1ao404";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearch(event) {
   event.preventDefault();
   let city = cityInput.value.trim();
